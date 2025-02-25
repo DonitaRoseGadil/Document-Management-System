5df537fbc8af4867315d3fc8f4cf6468b8b4f54c
DonitaRoseGadil <gadildonitarose@gmail.com>
2025-02-25 15:10:47 +0800

    newdb

--- database/lgu_dms.sql
+++ database/lgu_dms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2025 at 06:54 AM
+-- Generation Time: Feb 25, 2025 at 08:09 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,11 +44,7 @@ CREATE TABLE `minutes` (
 --
 
 INSERT INTO `minutes` (`id`, `no_regSession`, `date`, `genAttachment`, `resNo`, `title`, `type`, `status`, `attachment`) VALUES
-(1, '6th regular session', '2025-02-21', '', 'test1', 'test1', 'Draft', 'Approved', 0x75706c6f6164732f285041504552292050696c692d43616e617269756d2d4f766174756d2d50756c702d457874726163742d42696f2d426174746572792d4f706572617465642d506f7765722d42616e6b2e646f63782e706466),
-(2, '6th regular session', '2025-02-28', 0x285041504552292050696c692d43616e617269756d2d4f766174756d2d50756c702d457874726163742d42696f2d426174746572792d4f706572617465642d506f7765722d42616e6b2e646f63782e706466, 'Test2', 'Test2', 'Information', 'Information', 0x28),
-(3, '7th Regular Session', '2025-02-21', 0x28504150455229204164736f727074696f6e204361706162696c697479206f662054616c69736179205465726d696e616c696120436174617070612053656564204875736b2042696f6368617220666f722052656d6f76616c206f66204361646d69756d20696e20436f6e74616d696e617465642053796e746865746963205761746572202831292e706466, 'Test3', 'Test3', 'Referred to Committee', 'Referred to Committee', 0x5b),
-(4, '7th Regular Session', '2025-02-21', 0x28504150455229204164736f727074696f6e204361706162696c697479206f662054616c69736179205465726d696e616c696120436174617070612053656564204875736b2042696f6368617220666f722052656d6f76616c206f66204361646d69756d20696e20436f6e74616d696e617465642053796e746865746963205761746572202831292e706466, 'Test4', 'Test4', 'Approved', 'Approved', 0x31),
-(5, '7th Regular Session', '2025-02-25', 0x5f436c617373205363686564756c652e706466, 'Test5', 'Test5', 'Approved', 'Approved', 0x28);
+(2, '9th Regular Session', '2025-02-24', '', 'Test1', 'Test1', 'Draft', 'Draft', 0x75706c6f6164732f32323830352d41727469636c652d3237373832312d312d31302d32303231313132372e706466);
 
 -- --------------------------------------------------------
 
@@ -60,12 +56,21 @@ CREATE TABLE `ordinance` (
   `id` int(10) NOT NULL,
   `mo_no` varchar(20) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `d_adopted` date NOT NULL,
+  `date_adopted` date NOT NULL,
   `author_sponsor` varchar(100) NOT NULL,
-  `co_author` varchar(255) NOT NULL,
-  `remarks` varchar(20) NOT NULL,
-  `d_approved` date NOT NULL
+  `date_fwd` varchar(20) NOT NULL,
+  `date_signed` date NOT NULL,
+  `sp_approval` varchar(255) NOT NULL,
+  `attachment` longblob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ordinance`
+--
+
+INSERT INTO `ordinance` (`id`, `mo_no`, `title`, `date_adopted`, `author_sponsor`, `date_fwd`, `date_signed`, `sp_approval`, `attachment`) VALUES
+(10, 'MO1', '', '0000-00-00', '', '', '0000-00-00', '', ''),
+(11, '9', 'as', '2025-02-12', 'Josevhel', '2025-02-28', '2025-02-22', 'Approved - Thru LCE', 0x75706c6f6164732f285041504552292050696c692d43616e617269756d2d4f766174756d2d50756c702d457874726163742d42696f2d426174746572792d4f706572617465642d506f7765722d42616e6b2e646f63782e706466);
 
 -- --------------------------------------------------------
 
@@ -87,16 +92,6 @@ CREATE TABLE `resolution` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `resolution`
---
-
-INSERT INTO `resolution` (`id`, `reso_no`, `title`, `descrip`, `d_adopted`, `author_sponsor`, `co_author`, `remarks`, `d_approved`, `attachment`) VALUES
-(1, '044', 'A Resolution Adopting an Ordinance Enacting the Integrated Zoning Ordinance (ZO) of the Municipality of Talisay, Camarines Norte', 'sdfghjkljhgfds', '2019-05-27', 'Mass Motion', '', '', '2019-03-29', NULL),
-(3, '', 'Shuta ka', '', '0000-00-00', '', '', 'Draft', '0000-00-00', NULL),
-(4, '', 'isakhdhjfbkb', '', '0000-00-00', '', '', 'Referred to Committe', '0000-00-00', NULL),
-(5, '006', 'LGU TALISAY', 'shuta ka', '2025-02-13', 'Josevhel ', '', '', '2025-02-28', NULL);
-
---
 -- Indexes for dumped tables
 --
 
@@ -104,6 +99,12 @@ INSERT INTO `resolution` (`id`, `reso_no`, `title`, `descrip`, `d_adopted`, `aut
 -- Indexes for table `minutes`
 --
 ALTER TABLE `minutes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ordinance`
+--
+ALTER TABLE `ordinance`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -120,7 +121,13 @@ ALTER TABLE `resolution`
 -- AUTO_INCREMENT for table `minutes`
 --
 ALTER TABLE `minutes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `ordinance`
+--
+ALTER TABLE `ordinance`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `resolution`
