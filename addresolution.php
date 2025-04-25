093f86d4c468d736048c33560483ed987c5dc9e6
Arriana Mae Malubay <arimamalubay333@gmail.com>
2025-04-25 12:58:23 +0800

    Loading Screen

--- addresolution.php
+++ addresolution.php
@@ -3,22 +3,6 @@
 
 <?php include "header.php"; ?>
 
-<style>
-.spinner {
-    border: 8px solid #f3f3f3;
-    border-top: 8px solid #3498db;
-    border-radius: 50%;
-    width: 60px;
-    height: 60px;
-    animation: spin 1s linear infinite;
-}
-@keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-}
-</style>
-
-
 <body>
 
     <!--**********************************
@@ -619,10 +603,6 @@
     </script>
 
     <script>
-    document.getElementById("myForm").addEventListener("submit", function() {
-        document.getElementById("loadingOverlay").style.display = "block";
-    });
-
     document.getElementById("attachment").addEventListener("change", function () {
     const file = this.files[0];
         if (file) {
